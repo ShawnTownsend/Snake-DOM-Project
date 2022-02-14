0e5ba7c833ee353a0a2179547a6bc0a14a6d9b01
AndrewKins <andrewm.enoe@gmail.com>
2022-02-13 23:07:31 -0500

    made food spawn, might have a bug not sure yet

---
+++ food.js
@@ -0,0 +1,23 @@
+import k from './kaboom.js'
+function getRandomPosition(tileWidth = 16, tileHeight = 16) {
+    const tilesInX = Math.floor(k.width() / tileWidth);
+    const tilesInY = Math.floor(k.height()/ tileHeight);
+    //pixel location of the tile
+    const x = (Math.floor(k.rand(0, tilesInX)) * tileWidth) + (tileWidth * 0.5);
+    const y = (Math.floor(k.rand(0, tilesInY)) * tileHeight) + (tileHeight * 0.5);
+    return k.vec2(x, y);
+}
+export default function food () {   
+    return {
+        spawn () {
+            k.add([
+                k.pos(getRandomPosition()),
+                k.rect(16, 16),
+                k.color(0, 0, 255, 1),
+                k.origin('center'),
+                k.area(),
+                'food'
+            ])
+        }
+    }
+}
